ecacb2c7931bb87a26e2f90a2a63acf55bb99568
Rafiudeen <126104518+Rafi-deen@users.noreply.github.com>
2026-07-16 17:59:34 +0530

    fix border in latest news

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,60 +1,71 @@
-import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropright } from "react-icons/io"
 import { nanoid } from "nanoid"
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom"
 
-export default function LatestNews(){
+export default function LatestNews() {
+  const latestNews = [
+    {
+      category: "Politics",
+      date: "April 16, 2022",
+      title:
+        "Nicola Sturgeon Reported to Police Over Apparent Face Mask Law Breach",
+      description:
+        "Nicola Sturgeon has been reported to police after footage showed her apparently breaching Scotland's Covid...",
+    },
+    {
+      category: "Markets",
+      date: "April 17, 2022",
+      title: "A New Cruise Ship Is Redefining Luxury in the Galápagos",
+      description:
+        "South Africa's Eskom has begun implementing further power cuts from 4:00 p.m. local today untill..",
+    },
+    {
+      category: "Crypto",
+      date: "April 17, 2022",
+      title:
+        "Elon Musk Also Threatened to Buy My Company. Here's How We Handled It",
+      description:
+        "South Africa's Eskom has begun implementing further power cuts from 4:00 p.m. local today untill..",
+    },
+  ]
 
-    const latestNews = [
-      {
-        category: "Politics",
-        date: "April 16, 2022",
-        title:
-          "Nicola Sturgeon Reported to Police Over Apparent Face Mask Law Breach",
-        description: "Nicola Sturgeon has been reported to police after footage showed her apparently breaching Scotland's Covid..."
-      },
-      {
-        category: "Markets",
-        date: "April 17, 2022",
-        title: "A New Cruise Ship Is Redefining Luxury in the Galápagos",
-        description: "South Africa's Eskom has begun implementing further power cuts from 4:00 p.m. local today untill.."
-      },
-      {
-        category: "Crypto",
-        date: "April 17, 2022",
-        title:
-          "Elon Musk Also Threatened to Buy My Company. Here's How We Handled It",
-        description: "South Africa's Eskom has begun implementing further power cuts from 4:00 p.m. local today untill.."
-      }]
+  return (
+    <section className="latest-news-container mx-2">
+      <div className="flex items-center gap-1 font-bold text-xl mx-4">
+        <h1>Latest News</h1>
+        <IoIosArrowDropright />
+      </div>
 
-    return (
-        <section className="latest-news-container">
-            <div className="flex items-center gap-1 font-bold text-xl">
-                    <h1>
-                        Latest News 
-                    </h1>
-                    <IoIosArrowDropright />
+      <div className="border-x-[1px] border-custom-border px-4">
+        {latestNews.map((news, index) => {
+          //    console.log(news)
+          return (
+            <div key={nanoid()} className="grid py-2">
+              <div className="flex items-center gap-2 mb-2">
+                <span className="font-bold border border-4 border-custom-border px-2 py-1 text-xs">
+                  {news.category}
+                </span>
+                <span className="text-xs text-custom-clr1">{news.date}</span>
+              </div>
+              <h1 className="font-bold text-lg mb-2">{news.title}</h1>
+              <p className="mb-2 text-sm text-custom-para">
+                {news.description}
+              </p>
+              <NavLink
+                to="/"
+                className={({ isActive }) => `underline text-navlink font-bold}`}
+              >
+                Continue Reading
+              </NavLink>
+              {index == latestNews.length - 1 ? (
+                <></>
+              ) : (
+                <div className="border-b-3 border-custom-border border-dashed pt-4"></div>
+              )}
             </div>
-
-            {
-                latestNews.map(
-                    news => {
-                    //    console.log(news)
-                       return <div key={nanoid()} className="grid mt-4 mb-4 outline-right outline-1 outline-custom-border">
-                            <div className="flex items-center gap-2 mb-2">
-                                <span className="font-bold border border-2 border-custom-border px-2 py-1 text-xs">{news.category}</span>
-                                <span className="text-xs text-custom-clr1">{news.date}</span>
-                            </div>
-                            <h1 className="font-bold text-lg mb-2">
-                                {news.title}
-                            </h1>
-                            <p className="mb-2 text-sm text-custom-para">
-                                {news.description}
-                            </p>
-                            <NavLink to="/" className={({isActive}) => 'underline text-navlink font-bold' }>Continue Reading</NavLink>
-                        </div>
-                    }
-                )
-            }
-        </section>
-    )
+          )
+        })}
+      </div>
+    </section>
+  )
 }
